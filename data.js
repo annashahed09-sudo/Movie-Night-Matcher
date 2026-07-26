b7069db12231a085a511b586c0ec1f58753b9be0
Annanash.09 <annashahed09@gmail.com>
2026-07-26 13:47:18 -0400

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const drivers = [
         color: "#00a19c", 
         img: "https://formula1.com", 
         dry: 88, wet: 78, technical: 85, power: 90, 
-        history: [1, 3, 2, 1, 4]
+        history: [1, 2, 4, 1, 3]
     },
     { 
         name: "Lewis Hamilton", 
@@ -13,7 +13,7 @@ const drivers = [
         color: "#ef1a2d", 
         img: "https://formula1.com", 
         dry: 84, wet: 86, technical: 88, power: 85, 
-        history: [2, 1, 4, 3, 2]
+        history: [3, 1, 5, 2, 4]
     },
     { 
         name: "George Russell", 
@@ -21,7 +21,7 @@ const drivers = [
         color: "#00a19c", 
         img: "https://formula1.com", 
         dry: 82, wet: 76, technical: 80, power: 94, 
-        history: [4, 2, 1, 5, 3]
+        history: [2, 5, 1, 4, 2]
     },
     { 
         name: "Charles Leclerc", 
@@ -29,7 +29,7 @@ const drivers = [
         color: "#ef1a2d", 
         img: "https://formula1.com", 
         dry: 80, wet: 74, technical: 92, power: 86, 
-        history: [3, 5, 3, 2, 1]
+        history: [4, 3, 2, 5, 1]
     },
     { 
         name: "Lando Norris", 
@@ -37,7 +37,7 @@ const drivers = [
         color: "#ff8700", 
         img: "https://formula1.com", 
         dry: 83, wet: 80, technical: 84, power: 84, 
-        history: [5, 4, 5, 4, 5]
+        history: [5, 4, 3, 3, 5]
     },
     { 
         name: "Max Verstappen", 
@@ -45,7 +45,7 @@ const drivers = [
         color: "#0600ef", 
         img: "https://formula1.com", 
         dry: 78, wet: 95, technical: 90, power: 82, 
-        history: [6, 6, 6, 6, 6]
+        history: [6, 6, 6, 1, 3]
     }
 ];
 
